ca22c28c38f7b5999e628674d0facfc8c7dcfa67
David Ingram <david@ingramaviation.com>
2019-09-11 16:55:15 -0500

    file structure creTED BY GEM

---
+++ lib/example_proj.rb
@@ -0,0 +1,6 @@
+require "example_proj/version"
+
+module ExampleProj
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/example_proj/version.rb
@@ -0,0 +1,3 @@
+module ExampleProj
+  VERSION = "0.1.0"
+end
